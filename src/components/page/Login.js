3cac41c666126c25ff0b1ff429dfc83eb3181c0f
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-01-11 17:42:57 +0600

    login and sign up design

--- src/components/page/Login.js
+++ src/components/page/Login.js
@@ -4,15 +4,19 @@ import {
   Text,
   StyleSheet,
   Pressable,
+  TextInput,
+  ScrollView,
   TouchableOpacity,
 } from 'react-native';
 import {
-  TextInput,
   Provider as PaperProvider,
   Button,
   Title,
+  FAB,
 } from 'react-native-paper';
+import * as Animatable from 'react-native-animatable';
 import Feather from 'react-native-vector-icons/Feather';
+import {Icon, Content, Container} from 'native-base';
 
 export default function Login(props) {
   const [text, setText] = React.useState('');
@@ -21,55 +25,137 @@ export default function Login(props) {
     <View style={styles.container}>
       {/* logo section */}
       <View style={styles.logo_section}>
-        <Title>Idea House Shop</Title>
-      </View>
-      <View style={styles.formContainer}>
-        <TextInput
-          mode={'outlined'}
-          label="Email"
-          value={text}
-          onChangeText={(text) => setText(text)}
-          placeholder={'Email or Mobile Number'}
+        {/* <Title>Idea House Shop</Title> */}
+        <Animatable.Image
+          source={{
+            uri:
+              'https://cdn.iconscout.com/icon/free/png-256/google-148-189807.png',
+          }}
+          style={{
+            height: '100%',
+            width: '70%',
+          }}
+          animation="fadeInDownBig"
         />
-        <View style={{position: 'relative', marginTop: 10}}>
+      </View>
+      <Text style={[styles.iconColor, {textAlign: 'center', fontSize: 18}]}>Login</Text>
+      <View style={styles.formContainer}>
+        <View style={styles.authFormGroup}>
+          <View style={styles.appenedIcon}>
+            <Feather name="user" style={styles.iconColor} size={20} />
+          </View>
           <TextInput
-            mode={'outlined'}
-            secureTextEntry={secureTxtEntry}
-            label="Password"
+            mode={'flat'}
+            placeholder="Email"
+            placeholderTextColor={styles.iconColor}
             value={text}
+            style={styles.textInputStyle}
             onChangeText={(text) => setText(text)}
-            placeholder={'Password'}
+          />
+        </View>
+        <View style={[styles.authFormGroup, {marginTop: 10}]}>
+          <View style={styles.appenedIcon}>
+            <Feather name="lock" style={styles.iconColor} size={20} />
+          </View>
+          <TextInput
+            mode={'flat'}
+            secureTextEntry={secureTxtEntry}
+            placeholder="Password"
+            placeholderTextColor={styles.iconColor}
+            value={text}
+            style={styles.textInputStyle}
+            onChangeText={(text) => setText(text)}
           />
           <TouchableOpacity
-            style={{position: 'absolute', top: 20, right: 10}}
+            style={[
+              styles.appenedIcon,
+              styles.iconColor,
+              {right: 20, left: null},
+            ]}
             onPress={setSecureTxtEntry}>
             {secureTxtEntry ? (
-              <Feather name="eye-off" color="grey" size={20} />
+              <Feather name="eye-off" style={styles.iconColor} size={20} />
             ) : (
-              <Feather name="eye" color="grey" size={20} />
+              <Feather name="eye" style={styles.iconColor} size={20} />
             )}
           </TouchableOpacity>
         </View>
-        <Title style={{textAlign: 'right'}}>Forgot your Password?</Title>
-        <Button
-          icon="lock"
-          style={{padding: 10, backgroundColor: 'black'}}
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Login
-        </Button>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Pressable>
+            <Text style={[styles.iconColor, {fontSize: 15}]}>
+              Forgot your Password?
+            </Text>
+          </Pressable>
+          <FAB
+            style={styles.authRoundButton}
+            small
+            icon="arrow-right"
+            onPress={() => {
+              alert('hllworld');
+            }}
+          />
+        </View>
+      </View>
+      {/* sign in with */}
+      <View style={styles.footerSection}>
+        <View>
+          <Text style={[styles.iconColor, {fontSize: 16, marginBottom: 5}]}>
+            Login with
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Pressable
+            style={styles.socialIconHolder}
+            onPress={() => {
+              alert('heel owolr');
+            }}>
+            <Icon name="facebook" style={styles.socialIcon} type="Feather" />
+          </Pressable>
+          <Pressable
+            style={styles.socialIconHolder}
+            onPress={() => {
+              alert('heel owolr');
+            }}>
+            <Icon name="twitter" style={styles.socialIcon} type="Feather" />
+          </Pressable>
+          <Pressable
+            style={styles.socialIconHolder}
+            onPress={() => {
+              alert('heel owolr');
+            }}>
+            <Icon name="mail" style={styles.socialIcon} type="Feather" />
+          </Pressable>
+          <Pressable
+            style={styles.socialIconHolder}
+            onPress={() => {
+              alert('heel owolr');
+            }}>
+            <Icon name="linkedin" style={styles.socialIcon} type="Feather" />
+          </Pressable>
+        </View>
       </View>
       {/* sign up section */}
       <View style={styles.footerSection}>
-        <Title>
-          Don't have account?{' '}
+        <View style={{flexDirection: 'row'}}>
+          <Title>Don't have account?</Title>
           <Pressable
             onPress={() => {
               props.swithLoginSignUp('signup');
             }}>
-            <Text style={{color: '#a85032', fontSize: 18}}>Sign Up</Text>
+            <Text
+              style={[
+                styles.iconColor,
+                {fontSize: 18, marginLeft: 3, marginTop: 5},
+              ]}>
+              Sign Up
+            </Text>
           </Pressable>
-        </Title>
+        </View>
       </View>
     </View>
   );
@@ -85,7 +171,6 @@ const styles = StyleSheet.create({
   },
   formContainer: {
     paddingHorizontal: 20,
-    flexGrow: 1,
     justifyContent: 'center',
   },
   footerSection: {
@@ -93,8 +178,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   logo_section: {
-    flexBasis: '25%',
+    flexBasis: '20%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInputStyle: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginLeft: 50,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    color: '#81368f',
+  },
+  authFormGroup: {
+    flexDirection: 'row',
+    position: 'relative',
+    borderBottomWidth: 1,
+    borderBottomColor: '#81368f',
+    marginBottom: 35,
+  },
+  appenedIcon: {
+    position: 'absolute',
+    top: 0,
+    left: 10,
+    zIndex: 2,
+  },
+  iconColor: {
+    color: '#81368f',
+  },
+  authRoundButton: {
+    position: 'relative',
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#81368f',
+  },
+  socialIconHolder: {
+    padding: 10,
+    borderRadius: 25,
+    backgroundColor: '#81368f',
+    marginRight: 4,
+  },
+  socialIcon: {
+    color: '#fff',
+    fontSize: 15,
   },
 });
--- src/components/page/SignUp.js
+++ src/components/page/SignUp.js
@@ -5,16 +5,18 @@ import {
   StyleSheet,
   Pressable,
   TouchableOpacity,
-} from 'react-native';
-import {Container, Content} from 'native-base';
-import {
   TextInput,
+  Platform,
+} from 'react-native';
+import {Container, Icon, Content} from 'native-base';
+import {
   Provider as PaperProvider,
-  Button,
-  Title,
   Checkbox,
+  Title,
+  FAB,
 } from 'react-native-paper';
 import Feather from 'react-native-vector-icons/Feather';
+import * as Animatable from 'react-native-animatable';
 
 export default function SignUp(props) {
   const [text, setText] = React.useState('');
@@ -25,91 +27,184 @@ export default function SignUp(props) {
       <View style={styles.container}>
         {/* logo section */}
         <View style={styles.logo_section}>
-          <Title>Idea House Shop</Title>
+          {/* <Title>Idea House Shop</Title> */}
+          <Animatable.Image
+            source={{
+              uri:
+                'https://cdn.iconscout.com/icon/free/png-256/google-148-189807.png',
+            }}
+            style={{
+              height: '100%',
+              width: '50%',
+            }}
+            animation="fadeInDownBig"
+          />
         </View>
+        <Text style={[styles.iconColor, {textAlign: 'center', fontSize: 18}]}>
+          Sign Up
+        </Text>
         <View style={styles.formContainer}>
           <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
             <View style={{flexBasis: '48%'}}>
-              <TextInput
-                mode={'outlined'}
-                label="First Name"
-                value={text}
-                onChangeText={(text) => setText(text)}
-                placeholder={'First Name'}
-              />
+              <View style={[styles.authFormGroup, {marginTop: 10}]}>
+                <View style={styles.appenedIcon}>
+                  <Feather name="user" style={styles.iconColor} size={20} />
+                </View>
+                <TextInput
+                  placeholder="First Name"
+                  placeholderTextColor={styles.iconColor}
+                  style={styles.textInputStyle}
+                  value={text}
+                  onChangeText={(text) => setText(text)}
+                />
+              </View>
             </View>
             <View style={{flexBasis: '48%'}}>
-              <TextInput
-                mode={'outlined'}
-                label="Last Name"
-                value={text}
-                onChangeText={(text) => setText(text)}
-                placeholder={'Last Name'}
-              />
+              <View style={[styles.authFormGroup, {marginTop: 10}]}>
+                <View style={styles.appenedIcon}>
+                  <Feather name="user" style={styles.iconColor} size={20} />
+                </View>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholderTextColor={styles.iconColor}
+                  value={text}
+                  onChangeText={(text) => setText(text)}
+                  placeholder={'Last Name'}
+                />
+              </View>
             </View>
           </View>
-          <TextInput
-            mode={'outlined'}
-            label="Mobile Number"
-            value={text}
-            onChangeText={(text) => setText(text)}
-            placeholder={'Mobile Number'}
-          />
-          <TextInput
-            mode={'outlined'}
-            label="Email"
-            value={text}
-            onChangeText={(text) => setText(text)}
-            placeholder={'Email'}
-          />
-          <View style={{position: 'relative', marginTop: 10}}>
+          <View style={[styles.authFormGroup, {marginTop: 10}]}>
+            <View style={styles.appenedIcon}>
+              <Feather name="phone" style={styles.iconColor} size={20} />
+            </View>
             <TextInput
-              mode={'outlined'}
+              style={styles.textInputStyle}
+              placeholderTextColor={styles.iconColor}
+              value={text}
+              onChangeText={(text) => setText(text)}
+              placeholder={'Mobile Number'}
+            />
+          </View>
+          <View style={[styles.authFormGroup, {marginTop: 10}]}>
+            <View style={styles.appenedIcon}>
+              <Feather name="mail" style={styles.iconColor} size={20} />
+            </View>
+            <TextInput
+              style={styles.textInputStyle}
+              placeholderTextColor={styles.iconColor}
+              value={text}
+              onChangeText={(text) => setText(text)}
+              placeholder={'Email'}
+            />
+          </View>
+          <View style={[styles.authFormGroup, {marginTop: 10}]}>
+            <View style={styles.appenedIcon}>
+              <Feather name="lock" style={styles.iconColor} size={20} />
+            </View>
+            <TextInput
+              style={styles.textInputStyle}
+              placeholderTextColor={styles.iconColor}
               secureTextEntry={secureTxtEntry}
-              label="Password"
               value={text}
               onChangeText={(text) => setText(text)}
               placeholder={'Password'}
             />
             <TouchableOpacity
-              style={{position: 'absolute', top: 20, right: 10}}
+              style={[
+                styles.appenedIcon,
+                styles.iconColor,
+                {right: 20, left: null},
+              ]}
               onPress={setSecureTxtEntry}>
               {secureTxtEntry ? (
-                <Feather name="eye-off" color="grey" size={20} />
+                <Feather name="eye-off" style={styles.iconColor} size={20} />
               ) : (
-                <Feather name="eye" color="grey" size={20} />
+                <Feather name="eye" style={styles.iconColor} size={20} />
               )}
             </TouchableOpacity>
           </View>
 
-          <View style={{flexDirection: 'row'}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
             <Checkbox
               status={checked ? 'checked' : 'unchecked'}
               onPress={() => {
                 setChecked(!checked);
               }}
             />
-            <Title>I agree to the Privacy Policy and Terms & Conditions </Title>
+            <Text>I agree to the Privacy Policy and Terms & Conditions </Text>
           </View>
-          <Button
-            icon="lock"
-            style={{padding: 10, backgroundColor: 'black'}}
-            mode="contained"
-            onPress={() => console.log('Pressed')}>
-            Sign Up
-          </Button>
+          <View style={{alignItems: 'center'}}>
+            <FAB
+              style={styles.authRoundButton}
+              small
+              icon="arrow-right"
+              onPress={() => {
+                alert('hllworld');
+              }}
+            />
+          </View>
+        </View>
+        {/* sign in with */}
+        <View style={styles.footerSection}>
+          <View>
+            <Text style={[styles.iconColor, {fontSize: 16, marginBottom: 5}]}>
+              Sign Up with
+            </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Pressable
+              style={styles.socialIconHolder}
+              onPress={() => {
+                alert('heel owolr');
+              }}>
+              <Icon name="facebook" style={styles.socialIcon} type="Feather" />
+            </Pressable>
+            <Pressable
+              style={styles.socialIconHolder}
+              onPress={() => {
+                alert('heel owolr');
+              }}>
+              <Icon name="twitter" style={styles.socialIcon} type="Feather" />
+            </Pressable>
+            <Pressable
+              style={styles.socialIconHolder}
+              onPress={() => {
+                alert('heel owolr');
+              }}>
+              <Icon name="mail" style={styles.socialIcon} type="Feather" />
+            </Pressable>
+            <Pressable
+              style={styles.socialIconHolder}
+              onPress={() => {
+                alert('heel owolr');
+              }}>
+              <Icon name="linkedin" style={styles.socialIcon} type="Feather" />
+            </Pressable>
+          </View>
         </View>
         {/* sign up section */}
         <View style={styles.footerSection}>
-          <Title>
-            Already have an account?
+          <View style={{flexDirection: 'row'}}>
+            <Title>Don't have account?</Title>
             <Pressable
               onPress={() => {
                 props.swithLoginSignUp('login');
               }}>
-              <Text style={{color: '#a85032', fontSize: 18}}>Sign In</Text>
+              <Text
+                style={[
+                  styles.iconColor,
+                  {fontSize: 18, marginLeft: 3, marginTop: 5},
+                ]}>
+                Login
+              </Text>
             </Pressable>
-          </Title>
+          </View>
         </View>
       </View>
     </Container>
@@ -134,8 +229,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   logo_section: {
-    flexBasis: '25%',
+    flexBasis: '10%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  iconColor: {
+    color: '#81368f',
+  },
+  authFormGroup: {
+    flexDirection: 'row',
+    position: 'relative',
+    borderBottomWidth: 1,
+    borderBottomColor: '#81368f',
+    marginBottom: 15,
+  },
+  appenedIcon: {
+    position: 'absolute',
+    top: 0,
+    left: 10,
+    zIndex: 2,
+  },
+  textInputStyle: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginLeft: 50,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    color: '#81368f',
+  },
+  authRoundButton: {
+    position: 'relative',
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#81368f',
+  },
+  socialIconHolder: {
+    padding: 10,
+    borderRadius: 25,
+    backgroundColor: '#81368f',
+    marginRight: 4,
+  },
+  socialIcon: {
+    color: '#fff',
+    fontSize: 15,
   },
 });
