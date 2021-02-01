18e296a05696c0c6a5f48ff6b34f5730967fa737
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-02-01 18:41:04 +0600

    modal varient

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -42,17 +42,88 @@ import {UtilityContext} from '../providers/AppUtilityProvder';
 import {CartContext} from '../utils/CartContext';
 
 export default function CartModal() {
-  let [variationImages, setvariationImages] = useState([
-    'https://images.unsplash.com/photo-1533228100845-08145b01de14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=378&q=80',
-    'https://images.unsplash.com/photo-1586953208448-b95a79798f07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-    'https://images.unsplash.com/photo-1556155092-8707de31f9c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-  ]);
   const {halfModalVisible, setHalfModalVisible} = React.useContext(
     UtilityContext,
   );
   const {currentCartItem, setCurrentCartItem} = React.useContext(CartContext);
+  const [variantColor, setVariantColor] = React.useState([]);
+  const [variantSize, setVariantSize] = React.useState([]);
+  const [selectedColor, setSelectedColor] = React.useState('');
+  const [selectedSize, setSelectedSize] = React.useState('');
+  const [variantPrice, setVariantPrice] = React.useState([]);
+  const [productPrice, setProductPrice] = React.useState(0);
+  const [displayImage, setDisplayImage] = React.useState(null);
+  const [selectedImageIndex, setSelectedImageIndex] = React.useState(null);
+
+  const getVariantPrice = () => {
+    if (selectedColor !== '' && selectedSize !== '') {
+      if (variantPrice.length > 0) {
+        variantPrice.forEach((item) => {
+          if (item.size === selectedSize && item.color === selectedColor) {
+            setProductPrice(item.price);
+          }
+        });
+      }
+    }
+  };
+
   React.useEffect(() => {
-    console.log(currentCartItem, 'itemssss');
+    getVariantPrice();
+  }, [selectedColor, selectedSize]);
+
+  React.useEffect(() => {
+    let colors = [];
+    let sizes = [];
+    let var_prices = [];
+    if (currentCartItem.variants.length > 0) {
+      currentCartItem.variants.forEach((item, innd) => {
+        let singleSize = '';
+        if (item.opt1_name === 'size') {
+          singleSize = item.opt1_value;
+        } else if (item.opt2_name === 'size') {
+          singleSize = item.opt2_value;
+        }
+
+        if (singleSize !== '') {
+          if (sizes.indexOf(singleSize) === -1) {
+            sizes.push(singleSize);
+          }
+        }
+
+        let singlecolor = '';
+        if (item.opt2_name === 'color') {
+          singlecolor = item.opt2_value;
+        } else if (item.opt1_name === 'color') {
+          singlecolor = item.opt1_value;
+        }
+
+        if (singlecolor !== '') {
+          if (colors.indexOf(singlecolor) === -1) {
+            colors.push(singlecolor);
+          }
+        }
+        if (singlecolor !== '' && singleSize !== '') {
+          let variantPriceObj = {};
+          variantPriceObj.color = singlecolor;
+          variantPriceObj.size = singleSize;
+          variantPriceObj.price = item.price;
+          if (var_prices.indexOf(variantPriceObj) === -1) {
+            var_prices.push(variantPriceObj);
+          }
+        }
+      });
+
+      setVariantSize(sizes);
+      setVariantColor(colors);
+      setVariantPrice(var_prices);
+      setSelectedColor(colors[0]);
+      setSelectedSize(sizes[0]);
+      setDisplayImage(currentCartItem.feature_image_url);
+
+      setSelectedImageIndex(
+        currentCartItem.extra_images.indexOf(currentCartItem.feature_image_url),
+      );
+    }
   }, []);
   return (
     <Container>
@@ -64,13 +135,10 @@ export default function CartModal() {
             }}>
             <Col style={{flexDirection: 'row'}}>
               <View style={styles.imageContainer}>
-                <Image
-                  source={{uri: currentCartItem.feature_image_url}}
-                  style={AppStyle.image}
-                />
+                <Image source={{uri: displayImage}} style={AppStyle.image} />
               </View>
               <View style={styles.nameColor}>
-                <Text style={{fontSize: 30}}>${currentCartItem.price||0}</Text>
+                <Text style={{fontSize: 30}}>${productPrice}</Text>
                 <Text style={{fontSize: 20}}>{currentCartItem.name}</Text>
               </View>
             </Col>
@@ -85,35 +153,42 @@ export default function CartModal() {
             </Col>
           </Row>
           <Content style={styles.contentContainer}>
-            <Row>
-              <Col
-                style={{
-                  paddingVertical: 8,
-                  borderBottomColor: '#eee',
-                  borderBottomWidth: 1,
-                }}>
-                <View>
-                  <Text style={{fontSize: 20, paddingLeft: 10}}>
-                    Color Family
-                  </Text>
-                </View>
-                <View style={{flexDirection: 'row'}}>
-                  {variationImages.map((img, ind) => (
-                    <View
-                      key={ind}
-                      style={{width: 50, marginHorizontal: 10, height: 50}}>
-                      <Image style={AppStyle.image} source={{uri: img}} />
-                    </View>
-                  ))}
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
+            {currentCartItem.extra_images.length > 0 && (
+              <Row>
+                <Col
+                  style={{
+                    paddingVertical: 8,
+                    borderBottomColor: '#eee',
+                    borderBottomWidth: 1,
+                  }}>
                   <View>
-                    <Text>Quantity</Text>
+                    <Text style={{fontSize: 20, paddingLeft: 10}}>Images</Text>
                   </View>
+                  <View style={{flexDirection: 'row'}}>
+                    {currentCartItem.extra_images.map((img, ind) => (
+                      <Pressable
+                        key={ind}
+                        onPress={() => {
+                          setDisplayImage(img);
+                          setSelectedImageIndex(ind);
+                        }}
+                        style={
+                          selectedImageIndex === ind
+                            ? [styles.extra_images, styles.selectedItems]
+                            : styles.extra_images
+                        }>
+                        <Image style={AppStyle.image} source={{uri: img}} />
+                      </Pressable>
+                    ))}
+                  </View>
+                </Col>
+              </Row>
+            )}
+
+            <Row>
+              <Col style={{borderColor: '#eee', borderWidth: 1}}>
+                <View style={{height: 30}}>
+                  <Text style={{fontSize: 20, paddingLeft: 10}}>Quantity</Text>
                 </View>
               </Col>
               <Col>
@@ -122,76 +197,62 @@ export default function CartModal() {
                 </View>
               </Col>
             </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
+            {variantColor.length > 0 && (
+              <Row>
+                <Col>
                   <View>
-                    <Text>Quantity</Text>
+                    <Text style={{fontSize: 20, paddingLeft: 10}}>Color</Text>
                   </View>
-                </View>
-              </Col>
-              <Col>
-                <View style={{height: 30}}>
-                  <TouchSpin updateCount={() => {}} initialCount={1} />
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
+                  <View style={{flexDirection: 'row'}}>
+                    {variantColor.map((item, ind) => {
+                      return (
+                        <Pressable
+                          key={ind}
+                          onPress={() => {
+                            setSelectedColor(item);
+                            getVariantPrice();
+                          }}
+                          style={
+                            selectedColor === item
+                              ? [styles.variantBox, styles.selectedItems]
+                              : styles.variantBox
+                          }>
+                          <Text>{item}</Text>
+                        </Pressable>
+                      );
+                    })}
+                  </View>
+                </Col>
+              </Row>
+            )}
+            {variantSize.length > 0 && (
+              <Row>
+                <Col>
                   <View>
-                    <Text>Quantity</Text>
+                    <Text style={{fontSize: 20, paddingLeft: 10}}>Size</Text>
                   </View>
-                </View>
-              </Col>
-              <Col>
-                <View style={{height: 30}}>
-                  <TouchSpin updateCount={() => {}} initialCount={1} />
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
-                  <View>
-                    <Text>Quantity</Text>
+                  <View style={{flexDirection: 'row'}}>
+                    {variantSize.map((item, innd) => {
+                      return (
+                        <Pressable
+                          key={innd}
+                          onPress={() => {
+                            setSelectedSize(item);
+                            getVariantPrice();
+                          }}
+                          style={
+                            selectedSize === item
+                              ? [styles.variantBox, styles.selectedItems]
+                              : styles.variantBox
+                          }>
+                          <Text>{item}</Text>
+                        </Pressable>
+                      );
+                    })}
                   </View>
-                </View>
-              </Col>
-              <Col>
-                <View style={{height: 30}}>
-                  <TouchSpin updateCount={() => {}} initialCount={1} />
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
-                  <View>
-                    <Text>Quantity</Text>
-                  </View>
-                </View>
-              </Col>
-              <Col>
-                <View style={{height: 30}}>
-                  <TouchSpin updateCount={() => {}} initialCount={1} />
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{borderBottomColor: '#eee', borderBottomWidth: 1}}>
-                <View>
-                  <View>
-                    <Text>Quantity</Text>
-                  </View>
-                </View>
-              </Col>
-              <Col>
-                <View style={{height: 30}}>
-                  <TouchSpin updateCount={() => {}} initialCount={1} />
-                </View>
-              </Col>
-            </Row>
+                </Col>
+              </Row>
+            )}
           </Content>
           <Button style={{width: '100%', justifyContent: 'center'}}>
             <Text style={{color: '#fff', textAlign: 'center'}}>
@@ -240,5 +301,22 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     backgroundColor: '#fff',
+  },
+  variantBox: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginRight: 5,
+  },
+  selectedItems: {
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  extra_images: {
+    width: 50,
+    marginHorizontal: 10,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#000',
   },
 });
